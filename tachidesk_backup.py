5e4489f119be5b79e0e93baf6ed0c023e2d5cee3
Nishant <nishantchauhan949@hotmail.com>
2024-04-01 23:00:58 +0530

    loading env file for username and password

--- tachidesk_backup.py
+++ tachidesk_backup.py
@@ -1,12 +1,16 @@
+import os
 from re import findall
 from time import sleep
 
+from dotenv import load_dotenv
 from requests import get
 
 from delete_old_files import main_function
 from tachidesk_logger import get_logger
 
-WORK_DIR = '/home/nishant/tachidesk/tachidesk-backups/'
+load_dotenv()
+
+BACKUPS_WORKING_DIR = os.environ.get('BACKUPS_WORKING_DIR')
 
 
 def tachidesk_create_backup():
@@ -15,7 +19,10 @@ def tachidesk_create_backup():
         req = None
         try:
             for i in range(0, 3):
-                req = get(url='http://localhost:4567/api/v1/backup/export/file')
+                req = get(
+                    url='http://localhost:4567/api/v1/backup/export/file',
+                    auth=(os.environ.get('USERNAME'), os.environ.get('PASSWORD'))
+                )
                 if req.status_code == 200:
                     break
                 else:
@@ -28,14 +35,14 @@ def tachidesk_create_backup():
 
         file_name = findall("filename=\"(.+)\"", content_disposition)[0]
 
-        with open(file=f'{WORK_DIR + file_name}', mode='wb+') as tachidesk_backup:
+        with open(file=f'{BACKUPS_WORKING_DIR + file_name}', mode='wb+') as tachidesk_backup:
             tachidesk_backup.write(req.content)
 
         logger.info(
             f'tachidesk_backup.tachidesk_create_backup() :: {file_name} downloaded successfully :: {req.status_code}')
 
         # calling delete_old_files.main_function
-        main_function(path=WORK_DIR, extension='.gz', days='3')
+        main_function(path=BACKUPS_WORKING_DIR, extension='.gz', days='3')
 
     except Exception as e:
         logger.error(f'tachidesk_backup.tachidesk_create_backup() :: Some unexpected error occurred :: {e}')
